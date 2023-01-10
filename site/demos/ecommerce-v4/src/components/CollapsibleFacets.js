c9ed2ab7387ba9f8d7952d494a615177baf710b5
Shubham Shah <shubhamshahrising@gmail.com>
2023-01-10 18:51:56 +0530

    feat: treelist facet

--- site/demos/ecommerce-v4/src/components/CollapsibleFacets.js
+++ site/demos/ecommerce-v4/src/components/CollapsibleFacets.js
@@ -1,39 +1,21 @@
-import { ReactiveChart } from '@appbaseio/reactivesearch';
+import { TreeList } from '@appbaseio/reactivesearch';
 import { Collapse } from 'antd';
 
 const { Panel: CollapsePanel } = Collapse;
 
 export default function CollapsibleFacets({ isMobile }) {
 	return (
-		<Collapse
-			defaultActiveKey={
-				isMobile ? ['Category'] : ['Category', 'Sub-Category', 'Ratings', 'Color']
-			}
-		>
-			<CollapsePanel header={<h3>Category</h3>} key="Sub-Category">
-				<ReactiveChart
-					componentId="SubCategory"
-					dataField="categoryPath.name.keyword"
-					chartType="bar"
-					type="term"
+		<Collapse defaultActiveKey={['Category']}>
+			<CollapsePanel header={<h3>Category</h3>} key="Category">
+				<TreeList
+					componentId="Category"
+					showCount
+					title="TreeList UI"
+					showCheckbox
+					mode="multiple"
 					URLParams
-					useAsFilter
-					loader="Loading..."
-					setOption={(data) => {
-						let options = ReactiveChart.getOption(data);
-						let xAxis = options && options.xAxis;
-						let yAxis = options && options.yAxis;
-						xAxis.axisLabel = {
-							rotate: 90,
-							fontSize: 7,
-						};
-						yAxis.axisLabel = {
-							fontSize: 8,
-						};
-						options.xAxis = xAxis;
-						return options;
-					}}
-					react={{ and: ['Category', 'Color', 'SearchBox'] }}
+					dataField={['class.keyword', 'subclass.keyword']}
+					react={{ and: ['SubCategory', 'Color', 'SearchBox'] }}
 				/>
 			</CollapsePanel>
 		</Collapse>
